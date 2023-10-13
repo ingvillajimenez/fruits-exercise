3cbcd1e85443fa5b0c61b8c6396eb3e53d6c1da8
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-13 15:11:56 -0600

    Exercise Fruits Modules

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import foods from "./foods";
+import { choice, remove } from "./helpers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Randomly draw a fruit from the array
+let fruit = choice(foods);
+// Log the message "I'd like one RANDOMFRUIT, please."
+console.log(`I'd like one ${fruit}, please.`);
+// Log the message "Here you go: RANDOMFRUIT"
+console.log(`Here you go: ${fruit}`);
+// Log the message "Delicious! May I have another?"
+console.log("Delicious! May I have another?");
+// Remove the fruit from the array of fruits
+let remaining = remove(foods, fruit);
+// Log the message "I'm sorry, we're all out. We have FRUITLEFT left"
+console.log(
+  `I'm sorry, we're all out. We have ${remaining.length} other foods left`
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
